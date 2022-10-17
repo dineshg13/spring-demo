82b93eb3f3723fe934ba88826f266c72fcee0518
Dinesh Gurumurthy <dinesh.gurumurthy@datadoghq.com>
2022-10-16 21:09:34 -0400

    working version

--- src/main/java/com/dd/demo/DemoApplication.java
+++ src/main/java/com/dd/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.dd.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com.dd")
 public class DemoApplication {
 
 	public static void main(String[] args) {
