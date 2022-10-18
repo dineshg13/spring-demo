4c5492751ec145f2dafe8e35fd5c6db498a4d4e8
Dinesh Gurumurthy <dinesh.gurumurthy@datadoghq.com>
2022-10-18 14:50:02 -0400

    throw exception

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
- java -javaagent:opentelemetry-javaagent.jar \
+
+java -javaagent:opentelemetry-javaagent.jar \
       -Dotel.resource.attributes=service.name=your-service-name \
-      -Dotel.traces.exporter=logging\
-      -Dotel.metrics.exporter=logging\
       -jar ./build/libs/demo-0.0.1-SNAPSHOT.jar
+#      -Dotel.traces.exporter=logging\
+#      -Dotel.metrics.exporter=logging\
--- src/main/java/com/dd/controller/DemoController.java
+++ src/main/java/com/dd/controller/DemoController.java
@@ -1,17 +1,33 @@
 package com.dd.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class DemoController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam Integer num) {
-        return "Hello World!!";
-        
+    @GetMapping(path = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Map<String, String> hello(@RequestParam Integer num) {
+        if (num == 0) {
+            throw new RuntimeException("num is zero");
+        }
+        Map<String, String> map = new HashMap<>();
+        map.put("response", "Hello World!!" + num);
+        return map;
+
     }
 
+    @GetMapping("/api/foos")
+    @ResponseBody
+    public String getFoos(@RequestParam(defaultValue = "test") String id) {
+        return "ID: " + id;
+    }
 
 }
