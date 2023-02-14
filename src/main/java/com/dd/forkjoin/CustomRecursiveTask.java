df0b37586a2f1f18c8e9435f0991842c6f0f6753
Dinesh Gurumurthy <dinesh.gurumurthy@datadoghq.com>
2023-02-14 15:24:50 -0500

    add link attrs

--- src/main/java/com/dd/forkjoin/CustomRecursiveTask.java
+++ src/main/java/com/dd/forkjoin/CustomRecursiveTask.java
@@ -1,10 +1,10 @@
 package com.dd.forkjoin;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -13,56 +13,60 @@ import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 
 public class CustomRecursiveTask extends RecursiveTask<Integer> {
-    public static final String RECURSIVE_TASK = "recursive-task";
-    private static final int THRESHOLD = 2;
-    private int[] arr;
+  public static final String RECURSIVE_TASK = "recursive-task";
+  private static final int THRESHOLD = 2;
+  private int[] arr;
 
-    private Context context;
+  private Context context;
 
-    public CustomRecursiveTask(Context context, int[] arr ) {
-        this.context = context;
-        if (  this.context == null ){
-            this.context = Context.root();
-        }
-        this.arr = arr;
+  public CustomRecursiveTask(Context context, int[] arr) {
+    this.context = context;
+    if (this.context == null) {
+      this.context = Context.root();
     }
+    this.arr = arr;
+  }
 
-    @Override
-    protected Integer compute() {
-        if (arr.length > THRESHOLD) {
-            return ForkJoinTask.invokeAll(createSubtasks())
-                    .stream()
-                    .mapToInt(ForkJoinTask::join)
-                    .sum();
-        } else {
-            return processing(arr);
-        }
+  @Override
+  protected Integer compute() {
+    if (arr.length > THRESHOLD) {
+      return ForkJoinTask.invokeAll(createSubtasks()).stream().mapToInt(ForkJoinTask::join).sum();
+    } else {
+      return processing(arr);
     }
+  }
 
-    private Collection<CustomRecursiveTask> createSubtasks() {
-        Tracer tracer=GlobalOpenTelemetry.getTracer(RECURSIVE_TASK);
-        Span span =  tracer.spanBuilder("subtasks").addLink(Span.fromContext(this.context).getSpanContext())
-                .setAttribute("ar",arr.length)
-                .startSpan();
+  private Collection<CustomRecursiveTask> createSubtasks() {
+    Tracer tracer = GlobalOpenTelemetry.getTracer(RECURSIVE_TASK);
+    Span span =
+        tracer
+            .spanBuilder("subtasks")
+            .addLink(
+                Span.fromContext(this.context).getSpanContext(),
+                Attributes.builder().put("array-length", String.valueOf(arr.length)).build())
+            .startSpan();
 
-        Context ctx = Context.current().with(span);
-        List<CustomRecursiveTask> dividedTasks = new ArrayList<>();
-        dividedTasks.add(new CustomRecursiveTask(ctx,
-                Arrays.copyOfRange(arr, 0, arr.length / 2)));
-        dividedTasks.add(new CustomRecursiveTask(ctx,
-                Arrays.copyOfRange(arr, arr.length / 2, arr.length)));
-        span.end();
-        return dividedTasks;
-    }
+    Context ctx = Context.current().with(span);
+    List<CustomRecursiveTask> dividedTasks = new ArrayList<>();
+    dividedTasks.add(new CustomRecursiveTask(ctx, Arrays.copyOfRange(arr, 0, arr.length / 2)));
+    dividedTasks.add(
+        new CustomRecursiveTask(ctx, Arrays.copyOfRange(arr, arr.length / 2, arr.length)));
+    span.end();
+    return dividedTasks;
+  }
 
-    private Integer processing(int[] arr) {
-        Tracer tracer=GlobalOpenTelemetry.getTracer(RECURSIVE_TASK);
-        Span span =  tracer.spanBuilder("processing").addLink(Span.fromContext(this.context).getSpanContext())
-                .setAttribute("array.length",arr.length)
-                .startSpan();
-        int sum =  Arrays.stream(arr)
-                .sum();
-        span.end();
-        return sum ;
-    }
+  private Integer processing(int[] arr) {
+    System.out.println("in processing");
+    Tracer tracer = GlobalOpenTelemetry.getTracer(RECURSIVE_TASK);
+    Span span =
+        tracer
+            .spanBuilder("processing")
+            .addLink(
+                Span.fromContext(this.context).getSpanContext(),
+                Attributes.builder().put("array-length", String.valueOf(arr.length)).build())
+            .startSpan();
+    int sum = Arrays.stream(arr).sum();
+    span.end();
+    return sum;
+  }
 }
